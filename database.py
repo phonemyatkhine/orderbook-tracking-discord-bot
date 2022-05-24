1e030484efa2aecb124adcf291721f5cba7b10f6
Phone Myat Khine <phonemyatkhine88@gmail.com>
2022-05-24 23:42:51 +0200

    create/fetch

--- database.py
+++ database.py
@@ -1,24 +1,72 @@
 import sqlite3
+import traceback
+import sys
 
 class databaseConnection():
     def connect(self):
         try:
-            conn = sqlite3.connect('example.db')
+            self.conn = sqlite3.connect('order_book.db')
             print("Connection to database is successful")
-            return conn
-        except :
-            print("Connection to database is not successful")
+            return self.conn
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
 
-    def getCursor(self, conn):
-        cursor = conn.cursor()
-        return cursor
+    def getCursor(self):
+        try :
+            self.cursor = self.conn.cursor()
+            print("Cursor set")
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb)) 
     
-    def executeSql(self, cursor, sql):
-          
+    def executeSql(self, sql):
         try:
-            cursor.execute(sql)
-            print("Sql executed")
-        except:
-            print("Sql no executed")
+            self.cursor.execute(sql)
+            print("Sql executed" + sql)
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            return(traceback.format_exception(exc_type, exc_value, exc_tb))
      
+    def commit(self):
+        try:
+            self.conn.commit()
+            print("Commit success")
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
+            
+    def close(self):
+        try:
+            self.conn.close()
+            print("Closed")
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
+            
+    def fetchAll(self,sql):
+        try:
+            self.cursor.execute(sql)
+            return self.cursor.fetchall()
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
      
--- index.py
+++ index.py
@@ -7,19 +7,6 @@ from database import databaseConnection
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
-   
-connection = databaseConnection()
-conn = connection.connect()
-cursor = connection.getCursor(conn)
-# sql = '''   CREATE TABLE EMPLOYEE(
-#                     FIRST_NAME CHAR(20) NOT NULL,
-#                     LAST_NAME CHAR(20),
-#                     AGE INT, 
-#                     SEX CHAR(1),
-#                     INCOME FLOAT) ''' 
-
-sql = ''' DROP TABLE EMPLOYEE '''
-connection.executeSql(cursor,sql)
 
 @bot.command()
 async def test(ctx):
@@ -36,9 +23,81 @@ async def gmi(ctx,  member: discord.Member):
     
 @bot.command()
 async def new_collection(ctx,arg):
-    print(arg)
-    await ctx.channel.send("Making order book for new collection "+arg)
+    connection = databaseConnection()
+    connection.connect()
+    connection.getCursor()
+    split = arg.split("@")
+    # "insert into contacts (name, phone, email) values (?, ?, ?)",
+    #         (name, phone, email)
+    new_collection_sql = "INSERT INTO COLLECTIONS (NAME, TWITTER_LINK) VALUES ("+split[0]+","+split[1]+")"
+    print(new_collection_sql)
+    try :
+        connection.executeSql(new_collection_sql)
+        connection.commit()    
+        connection.close()
+        await ctx.channel.send("New collection " +split[0]+" created ")
+    except :
+        await ctx.channel.send("Collection creation failed")
 
+@bot.command()
+async def show_collections(ctx):
+    connection = databaseConnection()
+    connection.connect()
+    connection.getCursor()
+    show_collection_sql = "SELECT * FROM COLLECTIONS"
+    try :
+        collections = connection.fetchAll(show_collection_sql)
+        connection.commit()
+        connection.cursor()
+        await ctx.channel.send("Collections "+collections)
+    except :
+        await ctx.channel.send("Collection fetched failed")
+        
+@bot.command()
+async def make_tables(ctx):
+    connection = databaseConnection()
+    connection.connect()
+    connection.getCursor()
+    collections_sql = ''' CREATE TABLE COLLECTIONS(
+                            COLLECTION_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                            NAME VARCHAR(255) NOT NULL,
+                            TWITTER_LINK VARCHAR(255),
+                            DISCORD_LINK VARCHAR(255), 
+                            IMAGE_LINK VARCHAR(255) 
+                            ) '''
+    buy_orders_sql = ''' CREATE TABLE BUY_ORDERS(
+                            BUY_ORDER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                            COLLECTION_ID INT NOT NULL,
+                            DISCORD_ID VARCHAR(255) NOT NULL,
+                            PRICE FLOAT NOT NULL ,
+                            CONSTRAINT fk_collections
+                                FOREIGN KEY (collection_id)
+                                REFERENCES collections(id) 
+                        )'''
+    
+    sell_orders_sql = ''' CREATE TABLE SELL_ORDERS(
+                            SELL_ORDER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                            COLLECTION_ID INT NOT NULL,
+                            DISCORD_ID VARCHAR(255) NOT NULL,
+                            PRICE FLOAT NOT NULL ,
+                            CONSTRAINT fk_collections
+                                FOREIGN KEY (collection_id)
+                                REFERENCES collections(id) 
+                        )'''
+    
+    try :
+        connection.executeSql(collections_sql)
+        connection.executeSql(buy_orders_sql)
+        connection.executeSql(sell_orders_sql)
+        connection.commit()
+        connection.close()
+        await ctx.channel.send("Tables created successfully")
+    except :
+        await ctx.channel.send("Tables creation error")
+        
 bot.run(TOKEN)
+                      
 
 
+    
+                    
